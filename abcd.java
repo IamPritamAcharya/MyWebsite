677d84bdc57bfba594fdfde21ad7107cca60bfd2
[PritamAch] <[pritamacharya192@gmail.com]>
2024-05-28 21:22:24 +0530

    Update 1.1

--- abcd.java
+++
@@ -1,7 +0,0 @@
-
-
-class abcd {
-    public static void main(String[] args) {
-        System.out.println("HEllo");
-    }
-}
--- tempCodeRunnerFile.java
+++
@@ -1,5 +0,0 @@
-bcd {
-    public static void main(String[] args) {
-        System.out.println("HEllo");
-    }
-}
