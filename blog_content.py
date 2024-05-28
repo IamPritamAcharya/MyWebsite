cf624afa402376507836005d434d742a9ccd29f3
[PritamAch] <[pritamacharya192@gmail.com]>
2024-05-28 21:21:52 +0530

    Update 1.1

--- blog_content.py
+++ blog_content.py
@@ -1,11 +1,60 @@
 # Blog content
 blogs = {
-    "My Freshman Year In College": {
+    "A Year of Firsts: Lessons from My Freshman Year in College": {
         "image": "freshman.jpg",
         "snippet": "Things I learned in my 1st year of college",
         "content": """
-        # My Freshman Year In College
-        Since I don't have time I will update this blog by next week. Pinky Promise. For now have some Lorem Ipsum: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Morbi vel justo vitae lacus tincidunt ultrices.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Morbi vel justo vitae lacus tincidunt ultrices.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Morbi vel justo vitae lacus tincidunt ultrices.
+        # A Year of Firsts: Lessons from My Freshman Year in College
+        https://www.amazon.in/Subtle-Art-Not-Giving/dp/0062641549
+-> Adjusting Expectations\n
+After failing my JEE exams, I started college with pretty low expectations. My main goal was to avoid the mistakes I made during my drop year and focus on my studies. For a while, I managed to stick to that plan, but distractions inevitably crept in. Surprisingly, these diversions taught me lessons that my past self would never have understood.
+
+-> The Trap of Comparing Yourself to Others\n
+One of my favorite quotes from "The Subtle Art of Not Giving a F*ck" by Mark Manson is, “We’re apes. We think we’re all sophisticated with our toaster ovens and designer footwear, but we’re just a bunch of finely ornamented apes. And because we are apes, we instinctually measure ourselves against others and vie for status.” This really hit home for me. Growing up, I always thought that comparing myself to others was the only way to get better. But I realized that constantly comparing yourself to others only makes you feel inadequate. It's more productive to set your own goals and work towards them. What makes one person happy might not be the same for another. Focus on what matters to you.
+
+-> Dealing with Unfairness\n
+Life isn't always fair, and that can be tough to swallow. When you face unfair treatment, ask yourself if you deserved better. If you did, believe that things will balance out eventually. Karma has a way of catching up.
+
+-> Finding True Friends\n
+Al Capone once said, "I'd rather have 4 quarters than 100 pennies." During my freshman year, I met all sorts of people—some genuine, many not. True friends are those who put their selfishness aside for you and appreciate what you bring to the table. They’ll be there through thick and thin. Keep your distance from those who don't value you or who hide their true intentions.
+
+-> The Healing Power of Music\n
+Whenever I felt down, music was my go-to. It has an incredible way of lifting your spirits and improving your mood. Never underestimate its power.
+
+-> The Importance of Patience\n
+Patience is key. Good things take time, and learning to be patient is crucial. If you struggle with it, find ways to keep yourself busy.
+
+-> Honesty vs. Deception\n
+Being honest can feel like a rare quality these days. While lying sometimes feels necessary, it should only be for the right reasons, not to deceive or hide the truth. Trust, once broken, is hard to rebuild.
+
+-> The Value of Confrontation\n
+If someone’s actions hurt you, confront them. Honest confrontation can strengthen relationships, but if it ends a friendship, maybe that person wasn't right for you.
+
+-> Second Chances\n
+Everyone deserves a second chance, but if they keep making the same mistakes, it's unlikely they’ll change. Be careful with how many chances you give.
+
+-> The Reality of Looks\n
+Society often favors those with better looks, leading to discrimination based on appearance. While it’s important to present yourself well, remember that true worth goes beyond looks.
+
+-> Trusting Your Brain Over Your Heart\n
+In my experience, choosing logic over emotion has often led to better outcomes. Trust your intuition and reason.
+
+-> Being Humble and Calm\n
+Strive to be humble, calm, and logical. These qualities will make you charismatic and respected.
+
+-> Embracing College Life\n
+College is the perfect time to explore and say “Yes” to new experiences. Don’t be afraid to step out of your comfort zone. These experiences will shape your personality and future.
+
+-> Prioritizing Yourself\n
+Always put your needs first. While it’s great to help others, don’t forget to take care of yourself.
+
+-> Cherishing Family\n
+Your family will always be there for you. Cherish them and treat them with the respect they deserve. They are your true wealth.
+
+-> Choose Your Battles\n
+Finally, decide what matters to you and focus on that. Overthinking and stressing about everything will only drain you. Prioritize what truly deserves your attention and let go of the rest.
+
+Looking back on my freshman year, I've learned to set my own standards, value true friends, and embrace the journey with patience and humility. College is a transformative time, and these lessons have shaped who I am today.
         """
     },
     "How I learned to code": {
